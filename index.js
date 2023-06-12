7fc3e1b56ea8b2dc2afe92fe0bb3a00bccdae88b
nicchy123 <nasirchy252@.com>
2023-06-12 13:19:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 7000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbqqyyb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const categoriesCollection = client
+      .db("phone-resale")
+      .collection("category");
+
+    app.get("/", (req, res) => {
+      res.send("Phone resale server running");
+    });
+    
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Phone resale server running on port ${port}`);
+});
